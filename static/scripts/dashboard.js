7573f68edc0bb79d3b06fdb8b3417964001224c0
Soumyadip Roy <roysoumyadip334@gmail.com>
2024-06-26 17:50:37 +0530

    Delete static directory

--- static/scripts/dashboard.js
+++
@@ -1,8 +0,0 @@
-        function previewImage(input) {
-            const file = input.files[0];
-            const reader = new FileReader();
-            reader.onload = function (e) {
-                document.getElementById('avatar-img').src = e.target.result;
-            };
-            reader.readAsDataURL(file);
-        }
